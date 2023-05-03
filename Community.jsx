43cdc5c7724dab3582bc414ea55e7d6c974ae897
marcialarturo <87619763+marcialarturo@users.noreply.github.com>
2023-05-02 23:27:00 -0400

    add community

---
+++ Community.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Grid, Container, Center } from '@chakra-ui/react'
+
+import CardSession from './CardSession'
+
+function Community(props) {
+  return (
+    <div className="pb-8">
+      <Center>
+        <img src="./community.png" alt="community" />
+      </Center>
+    </div>
+  )
+}
+
+export default Community
